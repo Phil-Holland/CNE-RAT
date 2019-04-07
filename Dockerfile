d2ec683a274bc1c7317c70d600df4cdbb6d3ea45
AndreeaCristinaSmeu <a.smeu@warwick.ac.uk)>
2019-04-07 20:48:42 +0100

    IntaRNA stuff - build seems to work, but up fails

--- Dockerfile
+++ Dockerfile
@@ -11,6 +11,24 @@ RUN dpkg -i viennarna.deb
 RUN apt-get install -f -y
 RUN apt-get install -y texlive-extra-utils
 
+# install conda
+ENV LANG=C.UTF-8 LC_ALL=C.UTF-8
+ENV PATH /opt/conda/bin:$PATH
+ 
+RUN apt-get update --fix-missing && apt-get install -y wget bzip2 ca-certificates \
+    libglib2.0-0 libxext6 libsm6 libxrender1 \
+    git mercurial subversion
+ 
+RUN wget --quiet https://repo.anaconda.com/archive/Anaconda2-2018.12-Linux-x86_64.sh -O ~/anaconda.sh && \
+    /bin/bash ~/anaconda.sh -b -p /opt/conda && \
+    rm ~/anaconda.sh && \
+    ln -s /opt/conda/etc/profile.d/conda.sh /etc/profile.d/conda.sh && \
+    echo ". /opt/conda/etc/profile.d/conda.sh" >> ~/.bashrc && \
+    echo "conda activate base" >> ~/.bashrc
+ 
+#install inta
+RUN conda install -c conda-forge -c bioconda intarna
+
 # install python requirements
 RUN pip install -r requirements.txt
 
--- tasks/intarna.py
+++ tasks/intarna.py
@@ -1,8 +1,77 @@
-import sys, time
+import sys, time, subprocess, base64
 sys.path.append('..')
+from Bio import SeqIO
+from io import StringIO
 from app import celery 
 
+intarna_template = """
+# IntaRNA Tool Output
+
+CNE sequence:
+
+<pre>
+{cne}
+</pre>
+
+Query sequences:
+<pre>
+{seq}
+</pre>
+
+IntaRNA output:
+
+<pre>
+{output}
+</pre>
+
+"""
+
 @celery.task(name='intarna')
-def intarna(shared_cfg, tool_cfg):
-    time.sleep(1)
-    return str(shared_cfg)
+# def intarna(shared_cfg, tool_cfg):
+#     time.sleep(1)
+#     return str(shared_cfg)
+def intarna(config, working_dir):
+	#params - to get from config object in the future
+	cne_sequences_fasta = config['config']['cne']
+	query_sequences_fasta = config['config']['task_rna_rna_config']['query_sequences']
+	
+	cne_io = StringIO(cne_sequences_fasta)
+	cne_sequences_parsed = SeqIO.parse(cne_io, 'fasta')
+	cne_sequence = ''
+	for fasta in cne_sequences_parsed:
+		cne_sequence = str(fasta.seq)
+		break
+
+	query_io = StringIO(query_sequences_fasta)
+	query_sequences_parsed = SeqIO.parse(query_io, 'fasta')
+	query_sequences = []
+	for fasta in query_sequences_parsed:
+		query_sequences.append(str(fasta.seq))	
+
+	seq1 = query_sequences[0]
+
+	intarna_input = '-t {cne} -q {query} --energy=V'.format(
+		cne = cne_sequence,
+		query = seq1
+	)
+	print(intarna_input)
+
+	#run inta
+	p = subprocess.Popen(['IntaRNA', '-t', cne_sequence, '-q', seq1, '--energy=V'],
+		cwd = working_dir,
+		stdout = subprocess.PIPE,
+		stdin = subprocess.PIPE,
+		#env = inta
+	)
+	# output, err = p.communicate(input=str.encode(intarna_input))
+	output, err = p.communicate()
+	output = output.decode("utf-8")
+
+	return intarna_template.format(
+		cne = cne_sequence,
+		seq = ',\n\n'.join(query_sequences),
+		output = output
+	)
+
+
+
