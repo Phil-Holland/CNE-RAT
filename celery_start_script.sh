ff2aead2a88c9486189c31efaf50718886967ecf
Matt Reggler <m.reggler@warwick.ac.uk>
2019-05-03 01:11:38 +0100

    Export the env var to rpy2 can see R
    basically lets it `see` R_HOME

--- celery_start_script.sh
+++ celery_start_script.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
 rm /var/run/celery/*.pid
+
+export LD_LIBRARY_PATH="${LD_LIBRARY_PATH}:/usr/local/lib/R/lib"
+
 celery multi start $CELERY_WORKERS -A app.celery --pidfile="/var/run/celery/%n.pid" --logfile="/var/log/celery/%n%I.log"
 
 celery flower -A app.celery --loglevel=info --persistent=True --basic_auth=$FLOWER_USER:$FLOWER_PASSWORD
